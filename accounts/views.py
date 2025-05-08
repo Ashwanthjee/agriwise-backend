00f2f31e6b604e1881289570f24e9e7403dfc040
Ashwanthjee <ashwanthramesh31@gmail.com>
2025-05-08 23:40:34 +0530

    Clean backend only push

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
